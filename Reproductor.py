54b5d0e046e50fb8181c521bb642e59dd433c4cb
Sebastian <asprinos@uninorte.edu.co>
2022-10-04 13:20:20 -0500

    quinto commit

--- Reproductor.py
+++ Reproductor.py
@@ -1,6 +1,11 @@
 from tkinter import *
+import tkinter as tk
 ventana = Tk()
 ventana.title("Music Player")
-ventana.geometry("600x800")
-ventana.iconbitmap("gura.ico")
+ventana.geometry("800x600")
+ventana.iconbitmap(".image\gura.ico")
+ventana.config(bg="black")
+ventana.resizable(0,0)
+boton = tk.Button (text= ":D")
+boton.place(x=50, y=50 )
 ventana.mainloop()
